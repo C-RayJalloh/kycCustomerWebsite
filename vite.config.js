85726ba6a66214edac657afe52a72ac30df7e5ad
Jahrulo <mserayjay@gmail.com>
2024-10-23 11:55:24 +0000

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "kycCustomerWebsite",
+  base: "/kycCustomerWebsite",
   plugins: [react()],
   resolve: {
     alias: {
