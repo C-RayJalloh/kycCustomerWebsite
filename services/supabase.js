679ff5e5a558144e8af2c564191d29a400b3efc6
Jahrulo <mserayjay@gmail.com>
2024-10-24 12:47:15 +0000

    changes: updated

--- services/supabase.js
+++ services/supabase.js
@@ -4,7 +4,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = import.meta.env.VITE_DB_URL;
-const supabaseKey = import.meta.env.VITE_DB_KEY;
+const supabaseKey = import.meta.env.VITE_DB;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
